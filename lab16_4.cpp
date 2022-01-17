3eb42a72107637fc93a213eecb1f117237fae1f2
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2022-01-18 02:03:07 +0700

    164fn

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,35 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a , int &b , int &c , int &d){
+
+	int n[4] ;
+	int m[4] ;
+
+	int p = 0 , q = 0 , r = 0 , s = 0 ;
+	n[0] = a , n[1] = b , n[2] = c , n[3] = d ;
+	p = rand()%4 ;
+	m[0] = n[p] ;
+	n[p] = 0 ;
+	
+	do{
+		q = rand()%4 ;
+	}while(n[q] == 0) ;
+	m[1] = n[q] ;
+	n[q] = 0 ;
+
+	do{
+		r = rand()%4 ;
+	}while(n[r] == 0) ;
+	m[2] = n[r] ;
+	n[r] = 0 ;
+
+	do{
+		s = rand()%4 ;
+	}while(n[s] == 0) ;
+	m[3] = n[s] ;
+
+	a = m[0] , b = m[1] , c = m[2] , d = m[3] ;
+	
+	}
